c84fffe1f457515f34905492453a4f9ddf1e4d1e
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-01-28 21:13:57 +0800

    inertia svelte setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+import './../css/app.css';
+
+import { createInertiaApp } from '@inertiajs/svelte'
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
+        return pages[`./Pages/${name}.svelte`]
+    },
+    setup({ el, App, props }) {
+        new App({ target: el, props })
+    },
+})
---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="YukDatang">
+    <meta
+        content="Raktrek is a web-based platform designed to streamline and enhance the management and user experience of a library. It aims to provide users with a convenient way to explore the library's catalogue, borrow books, and manage their reading activities. In Indonesian, it's Sistem Informasi Perpustakaan"
+        name="description">
+    <meta content="raktrek,404 not found indonesia,sistem informasi perpustakaan" name="keywords">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="{{ asset('logo.jpg') }}"/>
+
+    <title>Raktrek</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title></title>
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+<body>
+@inertia
+</body>
+</html>
